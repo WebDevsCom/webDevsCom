5303f6dd842a2d72d74aa462c57bfcf44f62b851
Binu42 <kbinu42@gmail.com>
2020-05-13 03:37:44 +0530

    Updates

---
+++ precache-manifest.49a86e7a0cd097b9bc59df9e50e89418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e5c649676fb836df050b3d665c7551",
+    "url": "/WebDevsCom/webDevsCom.github.io/index.html"
+  },
+  {
+    "revision": "7149a5c5cd1f8c4398a6",
+    "url": "/WebDevsCom/webDevsCom.github.io/static/css/2.a2c18f5e.chunk.css"
+  },
+  {
+    "revision": "7cd0866b50e69095df51",
+    "url": "/WebDevsCom/webDevsCom.github.io/static/css/main.efb928b8.chunk.css"
+  },
+  {
+    "revision": "7149a5c5cd1f8c4398a6",
+    "url": "/WebDevsCom/webDevsCom.github.io/static/js/2.1e40ab35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebDevsCom/webDevsCom.github.io/static/js/2.1e40ab35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd0866b50e69095df51",
+    "url": "/WebDevsCom/webDevsCom.github.io/static/js/main.bd719395.chunk.js"
+  },
+  {
+    "revision": "c5e345271ae41903f0b9",
+    "url": "/WebDevsCom/webDevsCom.github.io/static/js/runtime-main.4167241c.js"
+  }
+]);
