a9dbd6ccaad47663f568c2be0e0add40e534fefc
Binu42 <kbinu42@gmail.com>
2020-10-29 05:04:19 +0530

    fixed styling related issues and remove repeated words :100:

--- src/components/Home/Contributor.js
+++ src/components/Home/Contributor.js
@@ -26,6 +26,42 @@ const Contributor = () => {
           className='image is-48x48 is-inline-flex'
           style={{ marginLeft: '-1rem' }}
         >
+          <a href='https://github.com/pkumars397'>
+            <img
+              className='is-rounded avatar-home'
+              alt='user-profile'
+              src='https://avatars0.githubusercontent.com/u/45540448?s=400&u=7ee96b166eba6a3552aef0f9496271c5b13adb7a&v=4'
+            />
+          </a>
+        </figure>
+        <figure
+          className='image is-48x48 is-inline-flex'
+          style={{ marginLeft: '-1rem' }}
+        >
+          <a href='https://github.com/VolailleInc'>
+            <img
+              className='is-rounded avatar-home'
+              alt='user-profile'
+              src='https://avatars1.githubusercontent.com/u/36109125?s=400&u=61d980bafcbcf617a189054bf3d5c9280340e9f9&v=4'
+            />
+          </a>
+        </figure>
+        <figure
+          className='image is-48x48 is-inline-flex'
+          style={{ marginLeft: '-1rem' }}
+        >
+          <a href='https://github.com/jaydeepkhatri'>
+            <img
+              className='is-rounded avatar-home'
+              alt='user-profile'
+              src='https://avatars0.githubusercontent.com/u/29619945?v=4'
+            />
+          </a>
+        </figure>
+        <figure
+          className='image is-48x48 is-inline-flex'
+          style={{ marginLeft: '-1rem' }}
+        >
           <a
             href='https://github.com/zoltanszogyenyi'
             target='_blank'
@@ -42,47 +78,11 @@ const Contributor = () => {
           className='image is-48x48 is-inline-flex'
           style={{ marginLeft: '-1rem' }}
         >
-          <a href='#!'>
+          <a href='https://github.com/harshloco'>
             <img
               className='is-rounded avatar-home'
               alt='user-profile'
-              src='https://randomuser.me/api/portraits/men/60.jpg'
-            />
-          </a>
-        </figure>
-        <figure
-          className='image is-48x48 is-inline-flex'
-          style={{ marginLeft: '-1rem' }}
-        >
-          <a href='#!'>
-            <img
-              className='is-rounded avatar-home'
-              alt='user-profile'
-              src='https://randomuser.me/api/portraits/men/70.jpg'
-            />
-          </a>
-        </figure>
-        <figure
-          className='image is-48x48 is-inline-flex'
-          style={{ marginLeft: '-1rem' }}
-        >
-          <a href='#!'>
-            <img
-              className='is-rounded avatar-home'
-              alt='user-profile'
-              src='https://randomuser.me/api/portraits/women/80.jpg'
-            />
-          </a>
-        </figure>
-        <figure
-          className='image is-48x48 is-inline-flex'
-          style={{ marginLeft: '-1rem' }}
-        >
-          <a href='#!'>
-            <img
-              className='is-rounded avatar-home'
-              alt='user-profile'
-              src='https://randomuser.me/api/portraits/men/10.jpg'
+              src='https://avatars2.githubusercontent.com/u/5271603?s=400&v=4'
             />
           </a>
         </figure>
