933e09dbdcef22e5d28fc0dec82ff16d2e7c43a4
Binu42 <kbinu42@gmail.com>
2021-02-10 23:39:12 +0530

    no data image

--- src/components/Resources/ResourceCards.js
+++ src/components/Resources/ResourceCards.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Star, GitPullRequest, Eye } from 'react-feather';
 import ReactTooltip from 'react-tooltip';
 import { Link, withRouter } from 'react-router-dom';
+import NoData from '../Utils/NoData';
 
 const ResourceCards = ({
   history,
@@ -18,156 +19,150 @@ const ResourceCards = ({
         style={{ margin: 'auto', animationDelay: '0.85s' }}
       >
         {filteredResources &&
-        filteredResources.length === 0 &&
-        window.location.pathname === '/resources' ? (
-          <p
-            className='subtitle is-h5 has-text-justified'
-            style={{ margin: '2rem auto 0', padding: '0 10px' }}
-          >
-            There is no resources present for searched Keyword "{searchInput}".
-            Please try searching for something else.
-          </p>
-        ) : (filteredResources &&
+          filteredResources.length === 0 &&
+          window.location.pathname === '/resources' ? (
+            <NoData text={`There is no resources present for searched Keyword ${searchInput}.
+            Please try searching for something else.`} />
+          ) : (filteredResources &&
             filteredResources.length === 0 &&
             window.location.pathname === '/bookmarked') ||
-          filteredResources === null ? (
-          <p
-            className='subtitle is-h5 has-text-justified'
-            style={{ margin: '2rem auto 0', padding: '0 10px' }}
-          >
-            {searchInput === '' && category === ''
-              ? 'You have not Bookmarked any Resources. Bookmark Resources to Read it later.'
-              : category !== ''
-              ? `You have not Bookmarked any Resources of category "${category}". Bookmark Resources to Read it later.`
-              : `You have not Bookmarked any resources with search keyword "${searchInput}"`}
-          </p>
-        ) : (
-          filteredResources.map((resource) => (
-            <div
-              key={resource.id}
-              className='column is-full-mobile is-half-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd'
-            >
-              <div
-                className='card box-shadow-lift'
-                style={{
-                  marginTop: '0.3rem',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: '100%',
-                }}
-              >
-                <header
-                  className='card-header'
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => history.push(`/resources/${resource.id}`)}
+            filteredResources === null ? (
+              <NoData
+                text={
+                  searchInput === '' && category === ''
+                    ? 'You have not Bookmarked any Resources. Bookmark Resources to Read it later.'
+                    : category !== ''
+                      ? `You have not Bookmarked any Resources of category "${category}". Bookmark Resources to Read it later.`
+                      : `You have not Bookmarked any resources with search keyword "${searchInput}"`
+                }
+              />
+            ) : (
+              filteredResources.map((resource) => (
+                <div
+                  key={resource.id}
+                  className='column is-full-mobile is-half-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd'
                 >
-                  <p className='card-header-title has-text-info subtitle is-5 is-capitalized'>
-                    {resource.repoName}
-                  </p>
-                </header>
-                <div className='card-content'>
-                  <div className='content'>
-                    <div
-                      className='media'
-                      style={{ marginBottom: '0', cursor: 'default' }}
+                  <div
+                    className='card box-shadow-lift'
+                    style={{
+                      marginTop: '0.3rem',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      height: '100%',
+                    }}
+                  >
+                    <header
+                      className='card-header'
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => history.push(`/resources/${resource.id}`)}
                     >
-                      <div className='media-left'>
-                        <figure className='image is-48x48 is-inline-flex'>
-                          <a
-                            href={`https://github.com/${resource.repoOwnerName}`}
-                            target='_blank'
-                            rel='noopener noreferrer'
-                          >
-                            <img
-                              className='is-rounded avatar-home'
-                              alt={resource.repoOwnerName}
-                              src={`https://avatars.githubusercontent.com/${resource.repoOwnerName}`}
-                            />
-                          </a>
-                        </figure>
-                      </div>
-                      <div className='media-content'>
-                        <p
-                          className='is-5 subtitle'
-                          style={{ marginBottom: '0' }}
+                      <p className='card-header-title has-text-info subtitle is-5 is-capitalized'>
+                        {resource.repoName}
+                      </p>
+                    </header>
+                    <div className='card-content'>
+                      <div className='content'>
+                        <div
+                          className='media'
+                          style={{ marginBottom: '0', cursor: 'default' }}
                         >
-                          {resource.repoOwner}
-                        </p>
+                          <div className='media-left'>
+                            <figure className='image is-48x48 is-inline-flex'>
+                              <a
+                                href={`https://github.com/${resource.repoOwnerName}`}
+                                target='_blank'
+                                rel='noopener noreferrer'
+                              >
+                                <img
+                                  className='is-rounded avatar-home'
+                                  alt={resource.repoOwnerName}
+                                  src={`https://avatars.githubusercontent.com/${resource.repoOwnerName}`}
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                          <div className='media-content'>
+                            <p
+                              className='is-5 subtitle'
+                              style={{ marginBottom: '0' }}
+                            >
+                              {resource.repoOwner}
+                            </p>
+                            <a
+                              href={`https://github.com/${resource.repoOwnerName}`}
+                              target='_blank'
+                              rel='noopener noreferrer'
+                            >
+                              @{resource.repoOwnerName}
+                            </a>
+                          </div>
+                        </div>
+                        <br />
+                        <div
+                          onClick={() => history.push(`/resources/${resource.id}`)}
+                          style={{ cursor: 'pointer' }}
+                          className='has-text-justified'
+                        >
+                          {resource.description}
+                        </div>
+                      </div>
+                    </div>
+                    <div style={{ marginTop: 'auto' }}>
+                      <div id='categories' className='card-content'>
+                        {resource.category.map((cat, index) => (
+                          <span
+                            data-tip={cat}
+                            key={index}
+                            onClick={() =>
+                              handleChangeInCategory(cat.toLowerCase())
+                            }
+                            id={cat === category ? 'active-dot' : ''}
+                            className={`category ${cat}`}
+                          ></span>
+                        ))}
+                      </div>
+                      <footer className='card-footer'>
                         <a
-                          href={`https://github.com/${resource.repoOwnerName}`}
+                          href={
+                            `https://github.com/${resource.repoOwnerName}` +
+                            '/' +
+                            resource.repoName +
+                            '/stargazers'
+                          }
                           target='_blank'
                           rel='noopener noreferrer'
+                          className='card-footer-item'
+                          data-tip='star this Repo'
                         >
-                          @{resource.repoOwnerName}
+                          <Star />
                         </a>
-                      </div>
-                    </div>
-                    <br />
-                    <div
-                      onClick={() => history.push(`/resources/${resource.id}`)}
-                      style={{ cursor: 'pointer' }}
-                      className='has-text-justified'
-                    >
-                      {resource.description}
-                    </div>
-                  </div>
-                </div>
-                <div style={{ marginTop: 'auto' }}>
-                  <div id='categories' className='card-content'>
-                    {resource.category.map((cat, index) => (
-                      <span
-                        data-tip={cat}
-                        key={index}
-                        onClick={() =>
-                          handleChangeInCategory(cat.toLowerCase())
-                        }
-                        id={cat === category ? 'active-dot' : ''}
-                        className={`category ${cat}`}
-                      ></span>
-                    ))}
-                  </div>
-                  <footer className='card-footer'>
-                    <a
-                      href={
-                        `https://github.com/${resource.repoOwnerName}` +
-                        '/' +
-                        resource.repoName +
-                        '/stargazers'
-                      }
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      className='card-footer-item'
-                      data-tip='star this Repo'
-                    >
-                      <Star />
-                    </a>
-                    <Link
-                      to={`/resources/${resource.id}`}
-                      className='card-footer-item'
-                      data-tip='view Resource'
-                    >
-                      <Eye /> &emsp;View
+                        <Link
+                          to={`/resources/${resource.id}`}
+                          className='card-footer-item'
+                          data-tip='view Resource'
+                        >
+                          <Eye /> &emsp;View
                     </Link>
-                    <a
-                      href={
-                        `https://github.com/${resource.repoOwnerName}` +
-                        '/' +
-                        resource.repoName +
-                        '/network/members'
-                      }
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      className='card-footer-item'
-                      data-tip='Fork this Repo'
-                    >
-                      <GitPullRequest />
-                    </a>
-                  </footer>
+                        <a
+                          href={
+                            `https://github.com/${resource.repoOwnerName}` +
+                            '/' +
+                            resource.repoName +
+                            '/network/members'
+                          }
+                          target='_blank'
+                          rel='noopener noreferrer'
+                          className='card-footer-item'
+                          data-tip='Fork this Repo'
+                        >
+                          <GitPullRequest />
+                        </a>
+                      </footer>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))
-        )}
+              ))
+            )}
       </div>
     </>
   );
---
+++ src/components/Utils/NoData.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import no_data from '../../assets/no_data.svg'
+
+const NoData = ({ text }) => {
+  return (
+    <div className="no-data">
+      <img className="no-data--image" src={no_data} alt="no data found" />
+      <p
+        className='is-size-4 is-size-3-desktop has-text-centered no-data--text'
+      >
+        {text}
+      </p>
+    </div>
+  )
+}
+
+export default NoData
