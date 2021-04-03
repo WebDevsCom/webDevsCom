2ea8f5471d96a12895a249416fab7e27033ac81b
Binu42 <kbinu42@gmail.com>
2021-04-03 23:45:16 +0530

    removed service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bulma/css/bulma.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/Utils/ScrollToTop';
@@ -16,5 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
